e9b8a603e342cd1b7a1535c1a71003d66035033e
brennoDuarte <brenno.gnr@gmail.com>
2018-09-25 19:51:32 -0300

    AV1 commit 2

--- AV1/AvaliacaoTopicosEspeciais/src/main/java/eleicao/resources/CandidatoResource.java
+++ AV1/AvaliacaoTopicosEspeciais/src/main/java/eleicao/resources/CandidatoResource.java
@@ -2,6 +2,7 @@ package eleicao.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class CandidatoResource {
 	private CandidatoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> listar(Integer id){
+	public ResponseEntity<?> listar(@PathVariable Integer id){
 		Candidato can = service.buscarCandidatos(id);
 		
 		return ResponseEntity.ok().body(can);
